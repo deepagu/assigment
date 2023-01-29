2f4fd50b107f0176589d8e6bed9631b907dc37b5
Immigration-Start-up <118651939+Immigration-Start-up@users.noreply.github.com>
2023-01-29 17:47:28 +0530

    Hello world

---
+++ myApp.js
@@ -0,0 +1,41 @@
+let express = require('express');
+let app = express();
+
+console.log("Hello world");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
